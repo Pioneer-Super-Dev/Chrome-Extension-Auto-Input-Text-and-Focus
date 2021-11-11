0275c0d2ec44fd5a4b642c3db8b75cc8b25a2a76
Huang-Bo <pioneer0221@outlook.com>
2021-11-10 22:03:35 -0800

    correct id name and tag name

--- content-script.js
+++ content-script.js
@@ -1,9 +1,2 @@
-//document.getElementById("inbox").value = "Hello";
-//document.getElementById("inbox").focus();
-//document.getElementsByTagName("input")[0].value = window.message;
-
-
-//chrome.storage.local.get("message", function(data) {
-    document.getElementsByTagName("input")[0].value = "Hello.";
-    document.getElementsByTagName("input")[0].focus();
-//});
+document.getElementById("message").value = "Hello";
+document.getElementById("message").focus();
